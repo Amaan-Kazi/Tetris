4eab2e1e52878473bec20d0cf7cfd0bed677f13d
Amaan Kazi <amaankazi.dev@gmail.com>
2026-06-20 15:54:06 +0530

    feat(util): tsc ticks_to_time

--- src/util/time.asm
+++ src/util/time.asm
@@ -67,6 +67,8 @@ msg10_len equ $ - msg10
 msg11     db 0x0A, 0x0A
 msg11_len equ $ - msg11
 
+testmsg   db "ns", 0x0A, 0x0A
+test_len  equ $ - testmsg
 
 section .text
 
@@ -189,6 +191,41 @@ calibrate_tsc_frequency:
   ; if debug_mode then write frequency and timing to debug file
   cmp byte [debug_mode], 1
   jne .exit
+
+  ; test ticks to time conversion
+  mov rdi, 5200000
+  mov byte [rbp - 76], 2
+  lea rsi, byte [rbp - 76]
+  call ticks_to_time
+
+  mov byte [testmsg], "n"
+  cmp byte [rbp - 76], 1
+  je .testing
+  mov byte [testmsg], "u"
+  cmp byte [rbp - 76], 2
+  je .testing
+  mov byte [testmsg], "m"
+  cmp byte [rbp - 76], 3
+  je .testing
+  mov byte [testmsg], " "
+
+  .testing:
+  mov rdi, rax
+  mov rsi, 0
+  lea rdx, [rbp - 96]
+  call num_to_ascii
+
+  mov rdx, rax
+  mov rax, 1
+  mov rdi, qword [debug_fd]
+  lea rsi, [rbp - 96]
+  syscall
+
+  mov rax, 1
+  mov rdi, qword [debug_fd]
+  lea rsi, testmsg
+  mov rdx, test_len
+  syscall
 
   mov rax, 1
   mov rdi, qword [debug_fd]
@@ -365,6 +402,129 @@ calibrate_tsc_frequency:
   mov rsi, msg11
   mov rdx, msg11_len
   syscall
+
+  .exit:
+  mov rsp, rbp
+  pop rbp
+  ret
+
+
+; arg1 rdi = uint64 ticks
+; arg2 rsi = uint8  &unit, 0 when you want closest unit, or specify required unit
+;
+; 0 = not specified
+; 1 = ns [nanoseconds]
+; 2 = us [microseconds]
+; 3 = ms [milliseconds]
+; 4 = s  [seconds]
+;
+; returns time converted from tsc ticks
+; returns unit in 1 byte address passed as arg2
+global ticks_to_time
+ticks_to_time:
+  push rbp
+  mov  rbp, rsp
+
+  movzx r11, byte [rsi]
+
+  ; jump to requested unit if any, or start comparing from seconds
+  cmp r11, 0
+  je .s_check
+  cmp r11, 1
+  je .ns
+  cmp r11, 2
+  je .us
+  cmp r11, 3
+  je .ms
+  jmp .s
+
+  ; seconds
+
+  .s_check:
+  cmp rdi, qword [tsc_ticks_per_s]
+  jl .ms_check
+
+  .s:
+    mov byte [rsi], 4
+
+    cmp qword [tsc_ticks_per_s], 0
+    je .s_per_tick
+
+    mov rdx, 0
+    mov rax, rdi
+    mov rcx, qword [tsc_ticks_per_s]
+    div rcx
+    jmp .exit
+
+    .s_per_tick:
+    mov rax, qword [tsc_s_per_tick]
+    mul rdi
+  jmp .exit
+
+  ; milliseconds
+
+  .ms_check:
+  cmp rdi, qword [tsc_ticks_per_ms]
+  jl .us_check
+
+  .ms:
+    mov byte [rsi], 3
+
+    cmp qword [tsc_ticks_per_ms], 0
+    je .ms_per_tick
+
+    mov rdx, 0
+    mov rax, rdi
+    mov rcx, qword [tsc_ticks_per_ms]
+    div rcx
+    jmp .exit
+
+    .ms_per_tick:
+    mov rax, qword [tsc_ms_per_tick]
+    mul rdi
+  jmp .exit
+
+  ; microseconds
+
+  .us_check:
+  cmp rdi, qword [tsc_ticks_per_us]
+  jl .ns
+
+  .us:
+    mov byte [rsi], 2
+
+    cmp qword [tsc_ticks_per_us], 0
+    je .us_per_tick
+
+    mov rdx, 0
+    mov rax, rdi
+    mov rcx, qword [tsc_ticks_per_us]
+    div rcx
+    jmp .exit
+
+    .us_per_tick:
+    mov rax, qword [tsc_us_per_tick]
+    mul rdi
+  jmp .exit
+
+  ; nanoseconds
+
+  .ns:
+    mov byte [rsi], 1
+
+    cmp qword [tsc_ticks_per_ns], 0
+    je .ns_per_tick
+
+    mov rdx, 0
+    mov rax, rdi
+    mov rcx, qword [tsc_ticks_per_ns]
+    div rcx
+    jmp .exit
+
+    .ns_per_tick:
+    mov rax, qword [tsc_ns_per_tick]
+    mul rdi
+  jmp .exit
 
   .exit:
   mov rsp, rbp
